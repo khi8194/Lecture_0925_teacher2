61ddfa4d59d7b687ca4d59b4de5cd80c5fb80d7f
tubi55 <tubi55@nate.com>
2024-09-24 17:04:41 +0900

    닫기 버튼 클릭시 아예 돔 요소 자체를 제거

--- youtube.js
+++ youtube.js
@@ -45,16 +45,22 @@ fetch(url)
   });
 
 //동적 생성요소에 이벤트 연결해서 동적으로 모달요소 추가
-document.body.addEventListener("click", function (e) {
+document.body.addEventListener("click", (e) => {
   if (e.target.className === "vidTitle") {
-    const asideEl = document.createElement("aside"); //엘리먼트 노드 직접 생성
-    //aside라는 비어있는 엘리먼트요소 안쪽에 기존처럼 innerHTML원하는 요소 동적 생성
+    const asideEl = document.createElement("aside");
     asideEl.innerHTML = `
       <div class='con'>
       </div>
-      <button>close</button>
+      <button class='btnClose'>close</button>
     `;
-    //append로 기존 요소 유지하면서 aside요소 추가 (인수로는 문자가 아닌 엘리먼트 노드 필요)
     document.body.append(asideEl);
+  }
+});
+
+//동적으로 생성된 모달 닫기버튼에 이벤트 위침
+document.body.addEventListener("click", (e) => {
+  if (e.target.className === "btnClose") {
+    //display:none과는 다르게 물리적으로 DOM자체를 제거
+    document.querySelector("aside").remove();
   }
 });
